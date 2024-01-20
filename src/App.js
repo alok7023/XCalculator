2c6559df6f00975d864b3aa5436b09791101a898
Alok Nayan <aloknayan@Aloks-MacBook-Air.local>
2024-01-20 15:25:26 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState("");
+  const buttons = [
+    "7",
+    "8",
+    "9",
+    "+",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "*",
+    "C",
+    "0",
+    "=",
+    "/"
+  ];
+  const calculateResult = () => {
+    try {
+      const calculateResult = eval(inputValue);
+      setResult(calculateResult);
+    } catch (e) {
+      setResult("Error");
+    }
+  }
+
+  const clearDisplay = () => {
+    setInputValue("");
+    setResult("");
+  }
+  const handleButtonClick = (number) => {
+    if (number === "=") {
+      calculateResult();
+    } else if (number === "C") {
+      clearDisplay();
+    } else {
+      setInputValue((prev) => prev + number);
+    }
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const key = event.key;
+      if (/[\d+*/=\-C]/.test(key)) {
+        handleButtonClick(key);
+      } else if (event.which === 13 || event.keyCode === 13) {
+        handleButtonClick("=");
+      } else if (key === "Backspace") {
+        setInputValue((prev) => prev.slice(0, -1));
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React Calculator</h1>
+      <input
+        type="text"
+        value={inputValue}
+        placeholder="0"
+        className="calc_input"
+        readOnly
+      />
+      <h3>Result : {result || "0"}</h3>
+      <div className='calc_btn'>
+        {buttons.map((number,i) => (
+          <button key = {i} onClick={() => handleButtonClick(number)}>
+            {number}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
